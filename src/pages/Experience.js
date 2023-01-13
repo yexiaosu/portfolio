d3cf01beb921349480e0f5a95bc93cab306c336b
yexiaosu <wang.lan313@sjtu.edu.cn>
2023-01-13 13:44:27 -0500

    feat: navbar and route

---
+++ src/pages/Experience.js
@@ -0,0 +1,67 @@
+import React from 'react';
+const Experience = () =>{
+  return (
+    <div id="main">
+        <h1>Experience &amp; Education</h1>
+        <div className="content">
+          <h2 className="section">Experience</h2>
+          <div>
+            <h3 className="title">Bilibili Inc.</h3>
+            <div className="time">12/2021-04/2022</div>
+            <div className="type">Server-side development intern</div>
+            <ul>
+              <li>
+                Joined Star Project Department, working on the web developing for
+                the international version of operation management platform.
+              </li>
+              <li>
+                Implemented the interfaces for front-end communicating with the
+                databases.
+              </li>
+              <li>
+                Completed the user interfaces for the operation management platform
+                by using existed components and writing JavaScript.
+              </li>
+              <li>
+                Designed and developed a way to manage and use current supported
+                languages during writing interfaces, which was easy to maintain.
+              </li>
+              <li>Organized and wrote some documents for current work.</li>
+            </ul>
+            <figure>
+              <img className="media" src="images/bilibili.jpg" alt="bilibili" />
+            </figure>
+          </div>
+          <div>
+            <h3 className="title">Sharewinfo</h3>
+            <div className="time">08/2020, 08/2021</div>
+            <div className="type">Data analytics intern</div>
+            <ul>
+              <li>Learn basic knowledge about database, SQL, and PowerBI.</li>
+              <li>Complete some small practice projects about data analysis.</li>
+            </ul>
+            <figure>
+              <img className="media" src="images/sharewinfo.jpg" alt="sharewinfo" />
+            </figure>
+          </div>
+          <h2 className="section">Education</h2>
+          <div>
+            <h3 className="title">University of Michigan, Ann Arbor, MI</h3>
+            <div className="time">08/2022 - Present</div>
+            <div className="type">Master of Science in Information</div>
+          </div>
+          <div>
+            <h3 className="title">Shanghai Jiao Tong University, Shanghai, China</h3>
+            <div className="time">09/2019 - Present</div>
+            <div className="type">B.S.E. in Electrical and Computer Engineering</div>
+          </div>
+          <div>
+            <h3 className="title">Qibao High School, Shanghai, China</h3>
+            <div className="time">09/2016 - 06/2019</div>
+            <div className="type">High School</div>
+          </div>
+        </div>
+      </div>
+  );
+}
+export default Experience;
---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+function Home() {
+  return (
+    <main id="main">
+        <h1>About Me</h1>
+        <div className="content">
+          <p>
+            I’m a first-year graduate student at UMSI, on the User-Centered Agile Development path. I majored in Electrical and Computer Engineering during my undergraduate learning process at Shanghai Jiao Tong University. Also, I have some experience in web development and data analytics because of a related internship.
+          </p>
+          <p>
+            I have many hobbies, including drawing, playing video games, making assembled models, etc. I am quite interested in drawing, though I have no professional training. For me, this hobby is my way of decompression and a manifestation of my love for life. If you are interested in my works, you can go to Gallery to see some of them.
+          </p>
+        </div>
+        <h1 className="formtitle">Contact Form</h1>
+        <form action="https://formspree.io/f/xayklaqn" method="POST">
+          <div className="firstname">
+            <label htmlFor="fname">First Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your first name" required />
+          </div>
+          <div className="lastname">
+            <label htmlFor="lname">Last Name</label>
+            <input type="text" id="lname" name="lastname" placeholder="Your last name" required />
+          </div>
+          <div className="sub">
+            <label htmlFor="subject">Subject</label>
+            <input type="text" id="subject" name="subject" placeholder="Subject of contact" required />
+          </div>
+          <div className="con">
+            <label htmlFor="content">Content</label>
+            <textarea id="content" name="content" placeholder="Write what you want to say to me!" required defaultValue={""} />
+          </div>
+          <input type="submit" defaultValue="Submit" />
+        </form>
+    </main>
+  );
+}
+
+export default Home;
