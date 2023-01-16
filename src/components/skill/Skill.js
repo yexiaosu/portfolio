9a4c85f3a6041f9336cc18ef006230d9152419d2
yexiaosu <wang.lan313@sjtu.edu.cn>
2023-01-16 00:05:44 -0500

    feat: skills

---
+++ src/components/skill/Skill.js
@@ -0,0 +1,34 @@
+import "./skill.css"
+import {BsPatchCheckFill} from "react-icons/bs"
+
+export default function Skills() {
+    return (
+      <div>
+        <h1>Skills</h1>
+        <div className="skills_container">
+            <Skill name="HTML" exp="advanced"></Skill>
+            <Skill name="CSS" exp="advanced"></Skill>
+            <Skill name="JavaScript" exp="experienced"></Skill>
+            <Skill name="SQL" exp="experienced"></Skill>
+            <Skill name="Python" exp="experienced"></Skill>
+            <Skill name="Golang" exp="intermediate"></Skill>
+            <Skill name="C" exp="experienced"></Skill>
+            <Skill name="C++" exp="advanced"></Skill>
+            <Skill name="MATLAB" exp="experienced"></Skill>
+            <Skill name="Verilog" exp="experienced"></Skill>
+        </div>
+      </div>
+    )
+}
+
+function Skill(props) {
+    return (
+        <article className="skills_detail">
+            <BsPatchCheckFill className="skills_detail_icon"></BsPatchCheckFill>
+            <div>
+            <h4>{props.name}</h4>
+            <h5>{props.exp}</h5>
+            </div>
+        </article>
+    )
+}
