35f420898338925ebd542a21eb0711150cfa6e8e
yexiaosu <wang.lan313@sjtu.edu.cn>
2023-01-21 13:12:46 -0600

    fix: remove wechat and update resume

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,7 +1,7 @@
 import "./card.css"
 import avatar from "../../assets/avatar.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWeixin, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
 export default function Card() {
@@ -15,12 +15,11 @@ export default function Card() {
           <div>M.S. in Information Science at UMich</div>
           <div>B.S.E. in ECE at SJTU</div>
           <div className="contact">
-            <CustomIcon icon={faWeixin} url="weixin://dl/chat?yexiaosu_busu"></CustomIcon>
             <CustomIcon icon={faGithub} url="https://github.com/yexiaosu"></CustomIcon>
             <CustomIcon icon={faLinkedin} url="https://www.linkedin.com/in/lan-wang-a753801a4/"></CustomIcon>
             <CustomIcon icon={faEnvelope} url="mailto:wanglanl@umich.edu"></CustomIcon>
           </div>
-          <button onClick={() => open("https://drive.google.com/file/d/1ZsMBQgv3lqtaOW8mFQVd323H_3WPm19q/view?usp=sharing")}>Resume</button>
+          <button onClick={() => open("https://drive.google.com/file/d/1GYS43LccPzq8JF72PvfTKE7I3a5t080X/view?usp=sharing")}>Resume</button>
         </div>
       </aside>
     )
